ec0719b10cb09e8c1db6bd933cd693f5a0bb3da4
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-03-04 16:46:00 +0900

    vercelの修正

--- student-site/src/app/login/page.js
+++ student-site/src/app/login/page.js
@@ -18,14 +18,16 @@ export default function LoginPage() {
         })
     }
   return (
-    <div className={styles.logincontainer}>
-        <h2>ログイン</h2>
-        <button className={styles.googlebtn} onClick={loginInWithGoogle}>
-            {/* <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google ロゴ"> */}
-            Googleでログイン
-        </button>
-        <p className={styles.ortext}>または</p>
-        <p>アカウントをお持ちでないですか？ <a href="#" className={styles.registerlink}>登録</a></p>
+    <div className={styles.container}>
+        <div className={styles.logincontainer}>
+            <h2>ログイン</h2>
+            <button className={styles.googlebtn} onClick={loginInWithGoogle}>
+                {/* <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" alt="Google ロゴ"> */}
+                Googleでログイン
+            </button>
+            <p className={styles.ortext}>または</p>
+            <p>アカウントをお持ちでないですか？ <a href="#" className={styles.registerlink}>登録</a></p>
+        </div>
     </div>
   )
 }
